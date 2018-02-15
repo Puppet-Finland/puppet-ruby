d5752072c5cbb1ddfc82266b662af50058e057b1
Samuli Seppänen <samuli@openvpn.net>
2018-02-15 14:13:14 +0200

    Fix net-ldap package name for good for RedHat
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

--- manifests/params.pp
+++ manifests/params.pp
@@ -11,7 +11,7 @@ class ruby::params {
     case $::osfamily {
         'RedHat': {
             $ruby_package_name = 'ruby'
-            $ruby_net_ldap_package_name = 'rubygems-net-ldap'
+            $ruby_net_ldap_package_name = 'rubygem-net-ldap'
         }
         'Debian': {
             $ruby_package_name = 'ruby'
